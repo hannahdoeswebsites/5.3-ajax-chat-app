fc8db68b9dfec2e1b894434155b8f14cc5576c7b
Hannah Jordan <hjorda0126@aol.com>
2015-06-04 08:53:03 -0400

    well, close enough. getting messages in javascript console. not on page.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -2,3 +2,9 @@ this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<h1>Hello</h1>\n";
 },"useData":true});
+this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<nav class=\"chatrooms\">\n  <ul>\n    <li>Chat 1</li>\n    <li>Chat 2 </li>\n    <li>Chat 3</li>\n  </ul>\n\n\n</nav>\n\n<section class=\"chat-box\">\n\n</section>\n\n<section class=\"outputMessage\">\n  <textarea form=\"text\" name=\"outputMessage\" cols=\"50\" rows=\"50\"> </textarea>\n  </section>\n\n<section class=\"enterMessage\">\n\n  <form class=\"input-messages\">\n<textarea class=\"message\" form=\"text\" name=\"enterMessage\" cols=\"50\" rows=\"2\" placeholder=\"Type your message here\"> </textarea>\n<input id=\"submit\" type=\"submit\" value=\"Submit Message\">\n</form>\n\n</section>\n";
+},"useData":true});
+this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<form class=\"login-form\">\n    <input class=\"login-form-username\" type=\"text\" placeholder=\"Username\">\n     <input id=\"submit\" type=\"submit\" value=\"Log In\">\n   </form>\n";
+},"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,87 @@
 (function(){
   'use strict';
 
+  var nameOfPerson = '';
+  var textEntered = '';
+  var currentTime = new Date();
+
+
+
   $(document).ready(function(){
-    $('body').prepend(JST['application']());
+
+    route();
+
+    $(document).on('submit', '.login-form', function(event){
+      event.preventDefault();
+      nameOfPerson = $(this).find('.login-form-username').val();
+      window.location.hash = '/chat';
+    });
+
+    $(window).on('hashchange', function(event){
+      event.preventDefault();
+      route();
+    });
   });
+
+  function route() {
+    switch(window.location.hash) {
+      case '':
+        $('.application').html(JST['login']());
+        break;
+      case '#/chat':
+        renderChat();
+
+    }
+  }
+
+  function renderChat() {
+    $('.application').html(JST['chat']());
+    console.log('username:', nameOfPerson);
+    $.ajax({
+      url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/"
+    }).then(function(data){
+    $('.application').html(JST['chat'](data));
+    console.log(data);
+      });
+
+
+
+  }
+  $(document).on('submit', '.input-messages', function(event){
+    event.preventDefault();
+    textEntered = $(this).find('.message').val();
+    console.log(nameOfPerson, currentTime, textEntered);
+    $.ajax({
+      url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/",
+      type: "POST",
+      data: {
+        username: nameOfPerson,
+        created_at: currentTime,
+        message: textEntered
+      }
+    });
+
+    function deleteInvalidMessages(){
+    $.ajax({
+      url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/"
+    }).then(function(messages) {
+      console.log(messages);
+      var invalid = _.reject(messages, function(message) {
+        return message.hasOwnProperty('username')
+            && message.hasOwnProperty('created_at')
+            && message.hasOwnProperty('content')
+      });
+      console.log(invalid);
+      invalid.forEach(function(message) {
+        $.ajax({
+          url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/" + message._id,
+          type: "DELETE"
+        })
+      });
+    });
+  }
+
+
+  });
+
 })();
